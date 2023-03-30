df3fd8f767e5e84275a26d2ad89fd505f9675a5f
Randi <dev@dochot.be>
2023-03-30 14:32:16 +0200

    add label

--- app.py
+++ app.py
@@ -4,29 +4,36 @@ from github import Github, GithubIntegration
 
 app = Flask(__name__)
 
-app_id = '<your_app_number_here>'
+app_id = ''
 
 # Read the bot certificate
 with open(
-        os.path.normpath(os.path.expanduser('bot_key.pem')),
+        os.path.normpath(os.path.expanduser('test-bot-umons.pem')),
         'r'
 ) as cert_file:
     app_key = cert_file.read()
-    
+
 # Create an GitHub integration instance
 git_integration = GithubIntegration(
     app_id,
     app_key,
 )
 
+
 def issue_opened_event(repo, payload):
     issue = repo.get_issue(number=payload['issue']['number'])
     author = issue.user.login
 
-    
+
+def add_label(repo, payload, label):
+    issue = repo.get_issue(number=payload['issue']['number'])
+    author = issue.user.login
+    issue.add_to_labels(label)
+
     response = f"Thanks for opening this issue, @{author}! " \
-                f"The repository maintainers will look into it ASAP! :speech_balloon:"
+               f"The repository maintainers will look into it ASAP! :speech_balloon:"
     issue.create_comment(f"{response}")
+
 
 @app.route("/", methods=['POST'])
 def bot():
@@ -47,9 +54,11 @@ def bot():
 
     # Check if the event is a GitHub issue creation event
     if all(k in payload.keys() for k in ['action', 'issue']) and payload['action'] == 'opened':
-        issue_opened_event(repo, payload)
+        # issue_opened_event(repo, payload)
+        add_label(repo, payload, 'pending')
 
     return "", 204
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
