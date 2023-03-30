6943ddefdb03464c17dfd7c3477fd08c700906b0
Randi <dev@dochot.be>
2023-03-30 15:37:15 +0200

    add merge

--- app.py
+++ app.py
@@ -35,6 +35,23 @@ def add_label(repo, payload, label):
     issue.create_comment(f"{response}")
 
 
+def create_issue(repo, payload, title, body):
+    issue = repo.create_issue(title=title, body=body)
+    issue.add_to_labels('pending')
+
+
+#create issue when pull request is closed
+def create_other_issue(repo, payload):
+    issue = repo.get_merge(payload['pull_request']['number'])
+    issue.create_issue_comment('A new pull request has been merge')
+
+
+# a pull request has been merged, remove branch
+def remove_branch(repo, payload):
+    branch = repo.get_branch(payload['pull_request']['number'])
+    branch.delete()
+
+
 @app.route("/", methods=['POST'])
 def bot():
     payload = request.json
@@ -52,10 +69,20 @@ def bot():
     )
     repo = git_connection.get_repo(f"{owner}/{repo_name}")
 
+
     # Check if the event is a GitHub issue creation event
     if all(k in payload.keys() for k in ['action', 'issue']) and payload['action'] == 'opened':
-        # issue_opened_event(repo, payload)
         add_label(repo, payload, 'pending')
+
+    elif all(k in payload.keys() for k in ['action', 'issue']) and payload['action'] == 'closed':
+        add_label(repo, payload, 'closed')
+
+    elif all(k in payload.keys() for k in ['action', 'pull_request']) and payload['action'] == 'closed':
+        remove_branch(repo, payload)
+
+    # check merge is true or false
+    elif all(k in payload.keys() for k in ['pull_request']) and payload["pull_request"]['merged'] == 'true':
+        create_other_issue(repo, payload)
 
     return "", 204
 
